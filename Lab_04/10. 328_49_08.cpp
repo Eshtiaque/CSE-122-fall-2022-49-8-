3f3dd6b7110fe04b542fb592c3181ad28810520a
Eshtiaque_Ahmed <109476726+Eshtiaque@users.noreply.github.com>
2022-08-11 22:29:34 +0600

    Add files via upload

---
+++ Lab_04/10. 328_49_08.cpp
@@ -0,0 +1,13 @@
+// 281A	Word Capitalization
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    s[0]=towupper(s[0]);
+    cout<<s;
+    return 0;
+
+}
